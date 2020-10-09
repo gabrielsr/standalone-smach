8f813a2f605836381943cb61b505eb9f1f8af57b
Gabriel Rodrigues <gabrielsr@gmail.com>
2020-10-09 22:30:10 +0200

    trying fix building.

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
 
-from setuptools import setup
-import generate_distutils_setup
+from setuptools import setup, find_packages
 
-d = generate_distutils_setup(
-   packages=['smach'],
-   package_dir={'': 'src'}
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="standalone-smach",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author="Gabriel Rodrigues",
+    author_email="gabrielsr@gmail.com",
+    description="Standalone fork of ros smach",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gabrielsr/standalone-smach",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
 )
-
-setup(**d)
